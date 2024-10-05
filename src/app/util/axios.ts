bc1cb13f67fe81dac1023620e766b1e89a0c8e70
Nutchanon Mongkonvilas <nutchanon.m@lakeikeda.com>
2024-10-06 00:05:10 +0700

    feat: setup axios and react query

---
+++ src/app/util/axios.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import { HOST_API } from '../global-config';
+
+// ----------------------------------------------------------------------
+
+const axiosInstance = axios.create({ baseURL: HOST_API });
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (error) =>
+    Promise.reject(
+      (error.response && error.response.data) || 'Something went wrong',
+    ),
+);
+
+// ----------------------------------------------------------------------
+
+export const endpoints = {
+  place: (placeName: string) => `/place/${placeName}`,
+  provinces: {
+    list: '/provinces',
+    dashboard: (provinceName: string) => `/province/${provinceName}`,
+    csv: '/provinces/csv',
+    places: (province: string) => `/province/${province}/places`,
+  },
+};
